0759277ac438069d15bc5908e8babac5456df1b3
mubee316 <145897332+mubee316@users.noreply.github.com>
2024-04-10 18:04:49 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,4 @@
+function add (a,b){
+    return add(a+b)
+}
+console.log(add(2,5))
---
+++ new.js
@@ -0,0 +1,63 @@
+// // // var firstName = "mubarak";
+// // // console.log(firstName);
+// // // var lastName = 'olalekan';
+// // // var age = 35;
+
+// // // let's define a person object
+
+// // var person = {
+// //     name: "Olalekan mubarak",
+// //     age: 23,
+// //     class: "jss-2",
+// //     maritalStatus: "divorced"
+// // }
+
+// // console.log(person.name) 
+// // // if statement
+// //  let x =10;
+// //  if (x > 19){
+// //     console.log("x is greater than 5")
+// //  }else{
+// //     console.log("x is not greater than 5")
+// //  }
+// // //  looops
+// // for(let i=0 ; i< 4; i++){
+// //     console.log("value of i:",i)
+// // }
+// // Function
+// // function greet(name){
+// // console.log("hello," + name +"!") 
+// // }
+// // greet("mubarak")
+// // if statement 2
+// // let n = "mubarak"
+// // if (n = "mubarak"){
+// //     console.log("ver wrong," +"answer:"+ n)
+// // }else{
+// //     console.log("very correct")
+// // }
+// for(let i = 20; i < 30 ; i++){
+//     console.log("the value of i :",i)
+// }
+// function help(name){
+//     console.log("hello,"+name+"!")
+// }
+// help("mubarak")
+try{
+function checkIfEven(num){
+if (num % 2 === 0){
+    console.log("true")
+ }//else {
+ //  console.log("false")
+//}
+}
+}catch(error){
+    console.error("thereis an error",error);
+}
+checkIfEven(19)
+// try {
+//     // Code that might throw an error
+//     throw new Error('This is an error!');
+//   } catch (error) {
+//     // Code to handle the error
+//   }
